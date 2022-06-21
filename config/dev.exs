a359b6f1ddd2c70b558e2f7dbdc9d5508a527827
Renato Valim <andradevalim.renato@gmail.com>
2022-06-21 16:38:11 -0300

    adding tailwind to project

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :sob_medida, SobMedidaWeb.Endpoint,
   secret_key_base: "BusewcbSXxBGzPk5cMB6NnystgUFZYmBZl+s7nXh+s3oEi1NaYlI5lgICOOsAhoe",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
